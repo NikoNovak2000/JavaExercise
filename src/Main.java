c22501c600e89943e9ad8805835e33335c8e546c
Niko Novak <novakniko2000@gmail.com>
2026-03-22 11:26:13 +0100

    Formatting numbers

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,16 @@
 import java.awt.*;
 import java.sql.SQLOutput;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        int result = Math.round(5.3F);
+        NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
+        String result = currencyInstance.format(1234567.891);
         System.out.println(result);
 
-        int result2 = (int)Math.ceil(5.3F);
+        String result2 = NumberFormat.getPercentInstance().format(0.32);;
         System.out.println(result2);
-
-        int result3 = (int)Math.floor(5.3F);
-        System.out.println(result3);
-
-        int result4 = Math.max(1,20);
-        System.out.println(result4);
-
-        int result5 = (int) (Math.random() * 100);
-        System.out.println(result5);
     }
 }
