9b82d2781164b1178400743a138051975201359a
Niko Novak <novakniko2000@gmail.com>
2026-03-22 10:20:22 +0100

    Strings and useful methods for strings

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,14 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now);
-
-        byte x = 1;
-        byte y = x;
-        x = 2;
-        System.out.println(x);
-        System.out.println(y);
-
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        point1.x = 3;
-        System.out.println(point1);
-        System.out.println(point2);
+        String message = "  Hello World  " + "!!";
+        boolean endsWithExclamation = message.endsWith("!!");
+        System.out.println(endsWithExclamation);
+        System.out.println(message.length());
+        System.out.println(message.indexOf("l"));
+        System.out.println(message.replace("!", "?"));
+        System.out.println(message.toLowerCase());
+        System.out.println(message.trim());
+        System.out.println(message);
     }
 }
