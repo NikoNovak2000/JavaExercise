f7a23fcf502898962c0f064a5c5c6e4a7d70b1a1
Niko Novak <novakniko2000@gmail.com>
2026-03-22 10:50:32 +0100

    Multidimensional arrays

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,11 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        System.out.println(Arrays.toString(numbers));
+        int[][] numbers = new int[2][3];
+        numbers[0][0] = 1;
+        System.out.println(Arrays.deepToString(numbers));
 
-        int[] otherNumbers = { 2, 3, 5, 1, 4};
-        System.out.println(otherNumbers.length);
-        Arrays.sort(otherNumbers);
-        System.out.println(Arrays.toString(otherNumbers));
+        int[][] moreNumbers = { {1, 2, 3}, { 4, 5, 6} };
+        System.out.println(Arrays.deepToString(moreNumbers));
     }
 }
