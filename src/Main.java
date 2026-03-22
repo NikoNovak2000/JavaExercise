60805d031e7798b846c295bffe6e70eb1b9d34f4
Niko Novak <novakniko2000@gmail.com>
2026-03-22 10:04:37 +0100

    Primitive and reference types difference

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
@@ -5,5 +6,17 @@ public class Main {
         byte age = 30;
         Date now = new Date();
         System.out.println(now);
+
+        byte x = 1;
+        byte y = x;
+        x = 2;
+        System.out.println(x);
+        System.out.println(y);
+
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 3;
+        System.out.println(point1);
+        System.out.println(point2);
     }
 }
