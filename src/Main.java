5ad9f35df45bd0b8c971c8f749d8ab71909c24ce
Niko Novak <novakniko2000@gmail.com>
2026-03-22 10:36:30 +0100

    Escape sequences

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 import java.awt.*;
+import java.sql.SQLOutput;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "  Hello World  " + "!!";
-        boolean endsWithExclamation = message.endsWith("!!");
-        System.out.println(endsWithExclamation);
-        System.out.println(message.length());
-        System.out.println(message.indexOf("l"));
-        System.out.println(message.replace("!", "?"));
-        System.out.println(message.toLowerCase());
-        System.out.println(message.trim());
+        // c:\Windows\...
+        String message = "Hello \"World\"";
+        String path = "c:\\Windows\\Desktop\\...";
+        String newLine = "c:\nWindows\nDesktop\n";
+        String tabLine = "c:\tWindows\tDesktop\t";
         System.out.println(message);
+        System.out.println(path);
+        System.out.println(newLine);
+        System.out.println(tabLine);
     }
 }
