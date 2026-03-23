69c42e590d459ab1f09f1d0c9b173e553702d627
unknown <novakniko2000@gmail.com>
2026-03-23 18:02:52 +0100

    Switch statements

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 public class Main {
     static void main(String[] args) {
-        int temperature = 30;
+        String role = "admin";
 
-        if (temperature > 30) {
-            System.out.println("It's a hot day.");
-            System.out.println("Drink more water.");
-        }
-        else if (temperature > 20) {
-            System.out.println("It's a nice day.");
-        }
-        else {
-            System.out.println("It's a cold day.");
+        switch (role) {
+            case "admin":
+                System.out.println("You are an admin.");
+                break;
+
+            case "moderator":
+                System.out.println("You are a moderator.");
+                break;
+
+            default:
+                System.out.println("You are a guest.");
         }
     }
 }
