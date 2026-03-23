bd1d714ba95cd7e4c92e8f7ae7717e73ce857315
unknown <novakniko2000@gmail.com>
2026-03-23 17:55:08 +0100

    If statements

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,16 @@
-import java.awt.*;
-import java.sql.SQLOutput;
-import java.text.NumberFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+    static void main(String[] args) {
+        int temperature = 30;
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter principal: ");
-        int principal = scanner.nextInt();
-
-        System.out.print("Enter annual interest rate: ");
-        float annualInterestRate = scanner.nextFloat();
-
-        System.out.print("Enter period (years): ");
-        byte periodInYears = scanner.nextByte();
-
-        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR; // monthly interest rate
-        float periodInMonths = periodInYears * MONTHS_IN_YEAR; // period in months
-        double mortgage = principal * ( monthlyInterestRate * (Math.pow(1 + monthlyInterestRate,periodInMonths)) / (Math.pow(1 + monthlyInterestRate, periodInMonths) - 1));
-
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage equals to: " + mortgageFormatted);
+        if (temperature > 30) {
+            System.out.println("It's a hot day.");
+            System.out.println("Drink more water.");
+        }
+        else if (temperature > 20) {
+            System.out.println("It's a nice day.");
+        }
+        else {
+            System.out.println("It's a cold day.");
+        }
     }
 }
-
-
-// Mortgage Calculator
-// 1st question: Principal/amount of loan (P)
-// 2nd question: Annual interest rate (r) divide by 100 because it's a percent and by 12 because we need monthly interest rate
-// 3rd question: Period (years) (n) * 12 for monthly
-// answer: mortgage (M)
-
-// M = P * ( r * (1 + r)pow n / (1 + r)pow n - 1)
---
+++ src/MortgageCalculator.java
@@ -0,0 +1,35 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class MortgageCalculator {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Enter annual interest rate: ");
+        float annualInterestRate = scanner.nextFloat();
+
+        System.out.print("Enter period (years): ");
+        byte periodInYears = scanner.nextByte();
+
+        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR; // monthly interest rate
+        float periodInMonths = periodInYears * MONTHS_IN_YEAR; // period in months
+        double mortgage = principal * ( monthlyInterestRate * (Math.pow(1 + monthlyInterestRate,periodInMonths)) / (Math.pow(1 + monthlyInterestRate, periodInMonths) - 1));
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage equals to: " + mortgageFormatted);
+    }
+}
+
+// Mortgage Calculator
+// 1st question: Principal/amount of loan (P)
+// 2nd question: Annual interest rate (r) divide by 100 because it's a percent and by 12 because we need monthly interest rate
+// 3rd question: Period (years) (n) * 12 for monthly
+// answer: mortgage (M)
+
+// M = P * ( r * (1 + r)pow n / (1 + r)pow n - 1)
