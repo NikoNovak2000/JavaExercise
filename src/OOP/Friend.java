4dbc341046501fa79d4ece9e09ea633ed5952351
Niko Novak <novakniko2000@gmail.com>
2026-03-24 21:42:15 +0100

    Static

---
+++ src/OOP/Friend.java
@@ -0,0 +1,19 @@
+package OOP;
+
+public class Friend {
+
+    String name;
+    static int numOfFriends;
+
+    Friend(String name){
+        this.name = name;
+        numOfFriends++;
+    }
+
+    static void showFriends(){
+        System.out.println("You have " + numOfFriends + " total friends!");
+    }
+}
+
+// static = makes a variable or method belong to the class rather than to any specific object.
+// Commonly used for utility methods or shared resources.
--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -3,19 +3,12 @@ package OOP;
 public class Main {
     static void main(String[] args) {
 
-        Car car1 = new Car("Mustang", "Red");
-        Car car2 = new Car("Qashqai", "Dark Blue");
-        Car car3 = new Car("Charger", "Yellow");
+        Friend friend1 = new Friend("Finn");
+        Friend friend2 = new Friend("Hector");
+        Friend friend3 = new Friend("Kenny");
+        Friend friend4 = new Friend("Mandy");
 
-        // Anonymous object Corvette
-        Car[] cars = {car1, car2, car3, new Car("Corvette", "Black")};
-
-        for(Car car : cars){
-            car.color = "black";
-        }
-
-        for(Car car : cars){
-            car.drive();
-        }
+        System.out.println(Friend.numOfFriends);
+        Friend.showFriends();
     }
 }
