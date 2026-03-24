404d57cb562ed5acb4e37700e1649d05dc20c43e
Niko Novak <novakniko2000@gmail.com>
2026-03-24 20:35:04 +0100

    Constructors

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -2,28 +2,28 @@ package OOP;
 
 public class Main {
     static void main(String[] args) {
+        Student student1 = new Student("Spongebob", 30, 3.2);
+        Student student2 = new Student("Patrick", 29, 2.2);
+        Student student3 = new Student("Squidward", 56, 4.1);
 
-        Car car = new Car();
+        System.out.println(student1.name);
+        System.out.println(student1.age);
+        System.out.println(student1.gpa);
+        System.out.println(student1.isEnrolled);
 
-        System.out.println(car.make);
-        System.out.println(car.model);
-        System.out.println(car.year);
-        System.out.println(car.price);
-        System.out.println(car.isRunning);
+        System.out.println(student2.name);
+        System.out.println(student2.age);
+        System.out.println(student2.gpa);
+        System.out.println(student2.isEnrolled);
 
-        System.out.println(car.isRunning);
-        car.start();
-        System.out.println(car.isRunning);
-        car.stop();
-        System.out.println(car.isRunning);
 
-        car.drive();
-        car.brake();
+        System.out.println(student3.name);
+        System.out.println(student3.age);
+        System.out.println(student3.gpa);
+        System.out.println(student3.isEnrolled);
 
-        Car car2 = new Car();
-        System.out.println(car2.model);
+        student1.study();
+        student2.study();
+        student3.study();
     }
 }
-
-// object = entity that holds data (attributes) and can perform actions (methods)
-// reference data type
---
+++ src/OOP/Student.java
@@ -0,0 +1,23 @@
+package OOP;
+
+public class Student {
+    String name;
+    int age;
+    double gpa;
+    boolean isEnrolled;
+
+    // Constructor
+    Student(String name, int age, double gpa){
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+        this.isEnrolled = true;
+    }
+
+    void study(){
+        System.out.println(this.name + "is studying.");
+    }
+}
+
+// constructor = special method to initialize objects
+// you can pass arguments to a constructor and set up initial values
