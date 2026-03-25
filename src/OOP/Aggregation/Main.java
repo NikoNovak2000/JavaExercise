67503825346c6a2d928006df251ba91d3b7079c9
Niko Novak <novakniko2000@gmail.com>
2026-03-25 10:27:29 +0100

    Aggregation

---
+++ src/OOP/Aggregation/Main.java
@@ -0,0 +1,24 @@
+package OOP.Aggregation;
+
+public class Main {
+    static void main(String[] args){
+
+        Book book1 = new Book("The Fellowship of the Ring", 423);
+        Book book2 = new Book("The Two Towers", 352);
+        Book book3 = new Book("The Return of the King", 416);
+
+        Book[] books = {book1, book2, book3};
+
+        for(Book book : books ){
+            System.out.println(book.displayInfo());
+        }
+
+        Library library = new Library("NYC Public Library", 1897, books);
+
+        library.displayInfo();
+
+    }
+}
+
+// Aggregation = Represents a "has-a" relationship between objects.
+// One object contains another object as part of its structure, but the contained object/s can exist independently.
--- src/OOP/Inheritance/Organism.java
+++ src/OOP/Inheritance/Organism.java
@@ -1,4 +1,4 @@
-package OOP.inheritance;
+package OOP.Inheritance;
 
 public class Organism {
 
