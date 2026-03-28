cd393e0b941f1abab5a0d3df38193405088d6546
Niko Novak <novakniko2000@gmail.com>
2026-03-28 11:10:59 +0100

    Enums

---
+++ src/OOP/Enumerations/Main.java
@@ -0,0 +1,32 @@
+package OOP.Enumerations;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a day of the week: ");
+        String response = scanner.nextLine().toUpperCase();
+
+        try{
+            Day day = Day.valueOf(response);
+
+            switch(day){
+                case MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY -> System.out.println("It is a weekday!");
+
+                case SATURDAY,SUNDAY -> System.out.println("It is a weekend!");
+            }
+        }
+        catch(IllegalArgumentException e){
+            System.out.println("Please enter a valid day");
+        }
+
+        scanner.close();
+    }
+}
+
+// Enums = Enumerations are a special kind of class that represents a fixed set of constants
+// They improve code readability, reliability and are easy to maintain
+// More efficient with switches when comparing Strings
